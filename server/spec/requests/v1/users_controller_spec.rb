b17270c7c9fdbcc746b6bc9a6a0568530c25eb08
Le Si Bich <le.si.bich@framgia.com>
2019-02-13 08:05:23 +0700

    fix

--- server/spec/requests/v1/users_controller_spec.rb
+++ server/spec/requests/v1/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe V1::UsersController, type: :request do
   describe 'POST /user' do
     it 'return status 200' do
-      post api_v1_user_path
+      post v1_user_path
       expect(response).to have_http_status(:ok)
     end
   end
