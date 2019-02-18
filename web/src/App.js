175aeebeff9ddc4793f98c4a0094b2c5fc8e6739
Le Si Bich <le.si.bich@framgia.com>
2019-02-18 07:38:24 +0700

    gco web

--- web/src/App.js
+++ web/src/App.js
@@ -1,85 +1,27 @@
 import React, { Component } from 'react'
-import axios from 'axios'
-
-function parseBase64(file) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
-    reader.onload = () => {
-      return resolve(reader.result)
-    }
-    reader.onerror = error => {
-      return reject(error)
-    }
-  })
-}
-
-async function mapAsync(list, mapper) {
-  const results = await Promise.all(list.map(item => mapper(item)))
-  return results
-}
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      content: '',
-      medias: [],
-    }
-
-    this.handleCreatePost = this.handleCreatePost.bind(this)
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleUploadFile = this.handleUploadFile.bind(this)
-  }
-
-  async handleCreatePost(event) {
-    event.preventDefault()
-
-    const medias = await mapAsync(this.state.medias, parseBase64)
-    const data = {
-      ...this.state,
-      medias,
-    }
-
-    axios
-      .post('http://localhost:3333/api/v1/tests', data, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      .then(response => console.log(response))
-      .catch(error => console.log(error))
-  }
-
-  handleInputChange(event) {
-    this.setState({ content: event.target.value })
-  }
-
-  async handleUploadFile(event) {
-    const { medias } = this.state
-
-    if (event.target.files[0]) {
-      this.setState({
-        medias: [...medias, event.target.files[0]],
-      })
-    }
-  }
-
   render() {
-    const { content } = this.state
-
     return (
       <div className="App">
-        <form action="http://localhost:3333/api/v1/tests" method="post">
-          <input type="text" name="content" />
-          Data
-          <br />
-          <input type="file" name="medias" />
-          File
-          <br />
-          <button type="submit">Submit</button>
-        </form>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit
+            <code>src/App.js</code>
+            and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     )
   }
